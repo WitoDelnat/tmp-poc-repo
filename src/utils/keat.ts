7dbb0dbcf8f0ecd051e2abf766fd8a91728fe7a5
Delnat Wito <wito.delnat@gmail.com>
2023-05-20 18:09:56 +0200

    add keat

---
+++ src/utils/keat.ts
@@ -0,0 +1,18 @@
+import { keatReact, audience, remoteConfig, rollouts } from "keat";
+
+const { useKeat, FeatureBoundary } = keatReact({
+  features: {
+    search: false,
+    redesign: false,
+    foo: true,
+    sortAlgorithm: {
+      variates: ["quicksort", "insertionSort", "heapsort"],
+    },
+  } as const,
+  plugins: [
+    remoteConfig("https://example.io/slowConfig", { interval: 300 }),
+    audience("staff", (user) => user.email?.endsWith("example.io")),
+    audience("preview", (user) => user.settings.previewEnabled),
+    rollouts(),
+  ],
+});
